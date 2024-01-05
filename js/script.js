b73056ecc8f3e182e4543f6f92db8a2a11e8e77d
Rhaíssa Lima <rhaissasbgl@gmail.com>
2024-01-04 21:56:28 -0300

    js perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do Menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
@@ -9,3 +10,35 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+    console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    pergunta.setAttribute('aria-expanded', 'true')
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains('ativa');
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
